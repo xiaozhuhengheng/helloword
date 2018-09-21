17ccabfb9a4a8027819794aac74444b7df76bd38
xiaoqiang lu <635849562@qq.com>
2018-09-21 17:44:18 +0800

    jainshao

--- src/com/lxq/Test.java
+++ src/com/lxq/Test.java
@@ -2,9 +2,6 @@ package com.lxq;
 
 public class Test {
 	public static void main(String[] args) {
-		System.err.println("sdf");
-		System.out.println("sddsd");
-		System.out.println("sdfds");
 		
 	}
 
